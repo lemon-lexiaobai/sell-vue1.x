61da372629c95de2725b3911120ce307efdff353
lemon-lexiaobai <17743692127@163.com>
2020-01-02 16:38:02 +0800

    vue1.x版本

---
+++ src/components/price/price.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="price">
+		<span class="now">￥{{ price }}</span>
+		<span class="old" v-show="oldPrice">￥{{ oldPrice }}</span>
+	</div>
+</template>
+
+<script type="text/javascript">
+	export default {
+		props: {
+			price: {
+				type: Number
+			},
+			oldPrice: {}
+		}
+	};
+</script>
+
+<style lang='stylus' rel='stylesheet/stylus'>
+	.price
+		font-weight: 700
+		line-height: 24px
+		font-size: 0
+		.now
+			margin-right: 8px
+			font-size: 14px
+			color: rgb(240,20,20)
+		.old
+			text-decoration: line-through
+			font-size: 10px
+			color: rgb(147,153,159)
+</style>
